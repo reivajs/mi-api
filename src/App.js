bd46b809f17682457fb06fb39e7389a9d93bdf43
Javier Saavedra <43253409+reivajs@users.noreply.github.com>
2022-09-27 20:52:59 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MiApi from './components/MiApi';
+
+function App() {
+  return (
+    <div className="container-fluid">
+     <h2 className='text-center'>React Search</h2>
+      <MiApi/>     
+    </div>
+  );
+}
+
+export default App;
